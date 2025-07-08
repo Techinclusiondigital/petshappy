1282721f2fa611ea3253e888aba082a611a459cc
Techinclusiondigital <techinclusiondigital@gmail.com>
2025-07-08 21:09:34 +0200

    datos

--- main.py
+++ main.py
@@ -975,8 +975,8 @@ def suscripcion_exitosa():
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
-print("Rutas registradas:")
-print(app.url_map)
+        print("Rutas registradas:")
+        print(app.url_map)
 
-app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
